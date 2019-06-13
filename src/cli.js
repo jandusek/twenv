ccb351fdb4ba09fd4f90ed2f6f757dcf8b3d0d9e
Jan Dusek <j@koiru.com>
2019-06-13 21:19:00 +0800

    Moved config to separate file, during switch undefine variables from previous env

--- src/cli.js
+++ src/cli.js
@@ -3,19 +3,9 @@ import { exec } from 'child_process';
 require('dotenv');
 
 // Individual environments and variables
-// that should be defined in each
-const envs = {
-  "Env1": {
-    "ACCOUNT_SID": "ACxxx",
-    "AUTH_TOKEN": "xxx",
-    "PHONE_NUMBER": "+1xxx"
-  },
-  "Env2": {
-    "ACCOUNT_SID": "ACyyy",
-    "AUTH_TOKEN": "yyy",
-    "PHONE_NUMBER": "+1yyy"
-  }
-}
+// that should be defined in each are read
+// from config.json
+let envs = require('./config.json');
 const default_env = "Env1";
 
 async function promptForEnv(options) {
@@ -37,24 +27,46 @@ async function promptForEnv(options) {
   };
 }
 
-function run_cmd(cmd) {
+function run_cmd(cmd, resolve) {
   const exec_options = { shell: '/usr/local/bin/fish' };
   exec(cmd, exec_options, (error, stdout, stderr) => {
     if (stderr) {
       console.log('stderr: ' + stderr);
+      resolve();
     }
     if (error !== null) {
       console.log('exec error: ' + error);
+      resolve();
     }
+    resolve();
   })
 }
 
 export async function cli(args) {
   let options = { environment: '' };
   options = await promptForEnv(options);
-  run_cmd(`set -xU TWENV ${options.environment}`);
-  Object.keys(envs[options.environment]).forEach((env_var) => {
-    run_cmd(`set -xU ${env_var} ${envs[options.environment][env_var]}`);
+  // unset the previous env variables
+  const old_env = process.env.TWENV;
+
+  let unsets = Object.keys(envs[old_env]).map((env_var) => {
+    return new Promise((resolve, reject) => {
+      //console.log(`Unsetting: ${env_var}`)
+      run_cmd(`set -eU ${env_var}`, resolve);
+    });
+  })
+
+  Promise.all(unsets).then(() => {
+    // set the new env variables
+    run_cmd(`set -xU TWENV ${options.environment}`, () => { });
+    let sets = Object.keys(envs[options.environment]).map((env_var) => {
+      return new Promise((resolve, reject) => {
+        //console.log(`Setting: ${env_var}`)
+        run_cmd(`set -xU ${env_var} ${envs[options.environment][env_var]}`, resolve);
+      });
+    });
+    Promise.all(sets).then(() => {
+      console.log(`Environment '${options.environment}' set.`);
+    });
   });
-  console.log(`Environment '${options.environment}' set.`);
+
 }
